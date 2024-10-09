4b82055e55e8f3e3323b484b4ca38418ec544c77
rados_stanisic <rados_stanisic@yahoo.com>
2024-10-09 15:05:35 +0200

    Dodao GetMovie test

--- Controllers/MovieControllerTest.cs
+++ Controllers/MovieControllerTest.cs
@@ -83,5 +83,44 @@ namespace CinemaAppTest.Controllers
             Assert.Empty(returnMovies);
         }
 
+        [Fact]
+        public async Task GetMovie_ReturnsOkResult_WithMovie_WhenMovieExists()
+        {
+            // Arrange
+            var mockMovie = new Movie
+            {
+                Title = "Movie1",
+                Genre = "Action",
+                Distributor = "Distributor1",
+                CountryOrigin = "USA",
+                Duration = 120,
+                ReleaseYear = 2020
+            };
+
+            _mockRepo.Setup(repo => repo.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(mockMovie);
+
+            // Act
+            var result = await _controller.GetMovie(1) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+            var returnMovie = Assert.IsType<Movie>(result.Value);
+            Assert.Equal("Movie1", returnMovie.Title);
+        }
+
+        [Fact]
+        public async Task GetMovie_ReturnsNotFoundResult_WhenMovieDoesNotExist()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Movie?)null);
+
+            // Act
+            var result = await _controller.GetMovie(1);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
     }
 }
--- obj/Debug/net7.0/CinemaAppTest.AssemblyInfo.cs
+++ obj/Debug/net7.0/CinemaAppTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a053136221073937dce2e83fc6df6f824f2a596c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
